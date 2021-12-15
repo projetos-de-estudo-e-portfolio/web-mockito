5b81ca992749f49bee6ce3e8304ed7a481dc10de
Luana da Rosa <lrosa.silva@outlook.com>
2021-12-15 15:34:34 -0300

    web mock application

---
+++ src/main/java/com/example/mockitoweb/GreetingService.java
@@ -0,0 +1,10 @@
+package com.example.mockitoweb;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String greet() {
+        return "Hello, World";
+    }
+}
---
+++ src/main/java/com/example/mockitoweb/MockitoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockitoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoWebApplication.class, args);
+	}
+
+}
